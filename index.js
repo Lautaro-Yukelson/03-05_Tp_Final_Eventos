91a0351ae113bda939f9935fa1da78416052947b
LAUTARO FERNANDO YUKELSON <47434208@est.ort.edu.ar>
2024-05-13 09:05:26 -0300

    Agrego el event router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import ProvinceRouter from './src/controllers/province-controller.js';
+import EventRouter from './src/controllers/event-controller.js'
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/province', ProvinceRouter);
+app.use('/api/event', EventRouter);
 
 app.listen(port, () => {
 	console.log('Aplicacion abierta en el puerto: ', port);
