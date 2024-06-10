eabfd22adf28e39320f1f7e052127959260740b8
LAUTARO FERNANDO YUKELSON <47434208@ort>
2024-06-10 11:58:33 -0300

    Todo el punto 8 y la puta madre

--- src/repositories/event-repository.js
+++ src/repositories/event-repository.js
@@ -5,192 +5,257 @@ const { Pool } = pkg;
 const pool = new Pool(config);
 
 export default class EventRepository {
-	async getEvents({ name, category, startdate, tag }) {
-		const client = await pool.connect();
-		try {
-			let sql =
-				'SELECT e.id, e.name AS event_name, e.description, e.start_date, e.duration_in_minutes, e.price, e.enabled_for_enrollment, e.max_assistance, ec.name AS category_name, t.name FROM events AS e INNER JOIN event_categories AS ec ON e.id_event_category = ec.id INNER JOIN event_tags AS et on e.id = et.id_event INNER JOIN tags as t on t.id = et.id_tag';
-			const values = [];
-			let cont = 1;
-			if (name != undefined) {
-				sql += ` AND e.name = $${cont}`;
-				cont++;
-				values.push(name);
-			}
-			if (category != undefined) {
-				sql += ` AND ec.name = $${cont}`;
-				cont++;
-				values.push(category);
-			}
-			if (startdate != undefined) {
-				sql += ` AND e.start_date = $${cont}`;
-				cont++;
-				values.push(startdate);
-			}
-			if (tag != undefined) {
-				sql += ` AND e.id IN (SELECT id_event FROM event_tags WHERE id_tag = (SELECT id FROM tags WHERE name = $${cont}))`;
-				cont++;
-				values.push(tag);
-			}
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getEvents({ name, category, startdate, tag }) {
+    const client = await pool.connect();
+    try {
+      let sql =
+        'SELECT e.id, e.name, e.description, e.start_date, e.duration_in_minutes, e.price, e.enabled_for_enrollment, e.max_assistance, ec.name AS category_name, ARRAY_AGG(t.name) AS tag_names FROM events AS e INNER JOIN event_categories AS ec ON e.id_event_category = ec.id LEFT JOIN event_tags AS et on e.id = et.id_event LEFT JOIN tags as t on t.id = et.id_tag GROUP BY e.id, e.name, e.description, e.start_date, e.duration_in_minutes, e.price, e.enabled_for_enrollment, e.max_assistance, ec.name;';
+      const values = [];
+      let cont = 1;
+      if (name != undefined) {
+        sql += ` AND e.name = $${cont}`;
+        cont++;
+        values.push(name);
+      }
+      if (category != undefined) {
+        sql += ` AND ec.name = $${cont}`;
+        cont++;
+        values.push(category);
+      }
+      if (startdate != undefined) {
+        sql += ` AND e.start_date = $${cont}`;
+        cont++;
+        values.push(startdate);
+      }
+      if (tag != undefined) {
+        sql += ` AND e.id IN (SELECT id_event FROM event_tags WHERE id_tag = (SELECT id FROM tags WHERE name = $${cont}))`;
+        cont++;
+        values.push(tag);
+      }
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getEventDetails(id) {
-		const client = await pool.connect();
-		try {
-			let sql =
-				"SELECT e.id, e.name, e.description, e.id_event_category, e.id_event_location, e.start_date, e.duration_in_minutes, e.price, e.enabled_for_enrollment, e.max_assistance, e.id_creator_user, json_build_object('id', el.id, 'id_location', el.id_location, 'name', el.name, 'full_address', el.full_address, 'max_capacity', el.max_capacity, 'latitude', el.latitude, 'longitude', el.longitude, 'id_creator_user', el.id_creator_user, 'location', json_build_object('id', l.id, 'name', l.name, 'id_province', l.id_province, 'latitude', l.latitude, 'longitude', l.longitude, 'province', json_build_object('id', p.id, 'name', p.name, 'full_name', p.full_name, 'latitude', p.latitude, 'longitude', p.longitude, 'display_order', p.display_order)), 'creator_user', (SELECT json_build_object('id', users.id, 'first_name', users.first_name, 'last_name', users.last_name, 'username', users.username, 'password', users.password) FROM users WHERE users.id = el.id_creator_user)) as event_location, array(SELECT json_build_object('id', t.id, 'name', t.name) FROM tags as t INNER JOIN event_tags as et on et.id_event = e.id and et.id_tag = t.id) as tags, (SELECT json_build_object('id', users.id, 'first_name', users.first_name, 'last_name', users.last_name, 'username', users.username, 'password', users.password) as creator_user FROM users WHERE users.id = e.id_creator_user), json_build_object('id', ec.id, 'name', ec.name, 'display_order', ec.display_order) as event_category FROM events as e INNER JOIN event_locations as el on el.id = e.id_event_location INNER JOIN locations as l on l.id = el.id_location INNER JOIN provinces as p on p.id = l.id_province INNER JOIN event_categories as ec on ec.id = e.id_event_category WHERE e.id = $1;";
-			const values = [id];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getEventDetails(id) {
+    const client = await pool.connect();
+    try {
+      let sql =
+        "SELECT e.id, e.name, e.description, e.id_event_category, e.id_event_location, e.start_date, e.duration_in_minutes, e.price, e.enabled_for_enrollment, e.max_assistance, e.id_creator_user, json_build_object('id', el.id, 'id_location', el.id_location, 'name', el.name, 'full_address', el.full_address, 'max_capacity', el.max_capacity, 'latitude', el.latitude, 'longitude', el.longitude, 'id_creator_user', el.id_creator_user, 'location', json_build_object('id', l.id, 'name', l.name, 'id_province', l.id_province, 'latitude', l.latitude, 'longitude', l.longitude, 'province', json_build_object('id', p.id, 'name', p.name, 'full_name', p.full_name, 'latitude', p.latitude, 'longitude', p.longitude, 'display_order', p.display_order)), 'creator_user', (SELECT json_build_object('id', users.id, 'first_name', users.first_name, 'last_name', users.last_name, 'username', users.username, 'password', users.password) FROM users WHERE users.id = el.id_creator_user)) as event_location, array(SELECT json_build_object('id', t.id, 'name', t.name) FROM tags as t INNER JOIN event_tags as et on et.id_event = e.id and et.id_tag = t.id) as tags, (SELECT json_build_object('id', users.id, 'first_name', users.first_name, 'last_name', users.last_name, 'username', users.username, 'password', users.password) as creator_user FROM users WHERE users.id = e.id_creator_user), json_build_object('id', ec.id, 'name', ec.name, 'display_order', ec.display_order) as event_category FROM events as e INNER JOIN event_locations as el on el.id = e.id_event_location INNER JOIN locations as l on l.id = el.id_location INNER JOIN provinces as p on p.id = l.id_province INNER JOIN event_categories as ec on ec.id = e.id_event_category WHERE e.id = $1;";
+      const values = [id];
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getEventLocation(id) {
-		const client = await pool.connect();
-		try {
-			let sql = 'SELECT * FROM event_locations WHERE id = $1';
-			const values = [id];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getEventLocation(id) {
+    const client = await pool.connect();
+    try {
+      let sql = 'SELECT * FROM event_locations WHERE id = $1';
+      const values = [id];
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getLocation(id) {
-		const client = await pool.connect();
-		try {
-			let sql = 'SELECT * FROM locations WHERE id = $1';
-			const values = [id];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getLocation(id) {
+    const client = await pool.connect();
+    try {
+      let sql = 'SELECT * FROM locations WHERE id = $1';
+      const values = [id];
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getProvince(id) {
-		const client = await pool.connect();
-		try {
-			let sql = 'SELECT * FROM provinces WHERE id = $1';
-			const values = [id];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getProvince(id) {
+    const client = await pool.connect();
+    try {
+      let sql = 'SELECT * FROM provinces WHERE id = $1';
+      const values = [id];
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getEventTags(eventId) {
-		const client = await pool.connect();
-		try {
-			let sql =
-				'select t.id, t.name from event_tags as et inner join tags as t on t.id = et.id_tag WHERE et.id_event = $1;';
-			const values = [eventId];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getEventTags(eventId) {
+    const client = await pool.connect();
+    try {
+      let sql =
+        'select t.id, t.name from event_tags as et inner join tags as t on t.id = et.id_tag WHERE et.id_event = $1;';
+      const values = [eventId];
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getCreatorUser(id) {
-		const client = await pool.connect();
-		try {
-			let sql = 'SELECT * FROM users WHERE id = $1';
-			const values = [id];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getCreatorUser(id) {
+    const client = await pool.connect();
+    try {
+      let sql = 'SELECT * FROM users WHERE id = $1';
+      const values = [id];
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getEventCategory(id) {
-		const client = await pool.connect();
-		try {
-			let sql = 'SELECT * FROM event_categories WHERE id = $1';
-			const values = [id];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getEventCategory(id) {
+    const client = await pool.connect();
+    try {
+      let sql = 'SELECT * FROM event_categories WHERE id = $1';
+      const values = [id];
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
 
-	async getEnrollments(
-		id,
-		{ first_name, last_name, username, attended, rating },
-	) {
-		const client = await pool.connect();
-		try {
-			let sql =
-				'SELECT u.first_name, u.last_name, u.username, ee.attended, ee.rating FROM users AS u INNER JOIN event_enrollments AS ee ON ee.id_user = u.id ';
-			const values = [];
-			let cont = 1;
-			if (first_name != undefined) {
-				sql += ` AND u.first_name = $${cont}`;
-				cont++;
-				values.push(first_name);
-			}
-			if (last_name != undefined) {
-				sql += ` AND u.last_name = $${cont}`;
-				cont++;
-				values.push(last_name);
-			}
-			if (username != undefined) {
-				sql += ` AND u.username = $${cont}`;
-				cont++;
-				values.push(username);
-			}
-			if (attended != undefined) {
-				//sql += ` WHERE u.id IN (SELECT id_user FROM event_enrollments WHERE attended = $${cont})`;
-				sql += ` AND ee.attended = $${cont}`;
-				cont++;
-				values.push(attended);
-			}
-			if (rating != undefined) {
-				//sql += ` WHERE u.id IN (SELECT id_user FROM event_enrollments WHERE rating >= $${cont})`;
-				sql += ` AND ee.rating >= $${cont}`;
-				cont++;
-				values.push(rating);
-			}
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+  async getEnrollments(
+    id,
+    { first_name, last_name, username, attended, rating }
+  ) {
+    const client = await pool.connect();
+    try {
+      let sql =
+        'SELECT u.first_name, u.last_name, u.username, ee.attended, ee.rating FROM users AS u INNER JOIN event_enrollments AS ee ON ee.id_user = u.id ';
+      const values = [];
+      let whereClause = false;
 
-	async addEvent(event) {
-		const client = await pool.connect();
-		try {
-			let sql =
-				'INSERT INTO public.events(name, description, id_event_category, id_event_location, start_date, duration_in_minutes, price, enabled_for_enrollment, max_assistance, id_creator_user, max_capacity)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);';
-			const values = [
-				event.name,
-				event.description,
-				event.id_event_category,
-				event.id_event_location,
-				event.start_date,
-				event.duration_in_minutes,
-				event.price,
-				event.enabled_for_enrollment,
-				event.max_assitance,
-				event.max_capacity,
-				event.user.id,
-			];
-			const result = await client.query(sql, values);
-			return result.rows;
-		} finally {
-			client.release();
-		}
-	}
+      if (first_name !== undefined) {
+        sql += `WHERE u.first_name = $1 `;
+        values.push(first_name);
+        whereClause = true;
+      }
+
+      if (last_name !== undefined) {
+        if (whereClause) sql += 'AND ';
+        else sql += 'WHERE ';
+        sql += `u.last_name = $${values.length + 1} `;
+        values.push(last_name);
+        whereClause = true;
+      }
+
+      if (username !== undefined) {
+        if (whereClause) sql += 'AND ';
+        else sql += 'WHERE ';
+        sql += `u.username = $${values.length + 1} `;
+        values.push(username);
+        whereClause = true;
+      }
+
+      if (attended !== undefined) {
+        if (whereClause) sql += 'AND ';
+        else sql += 'WHERE ';
+        sql += `ee.attended = $${values.length + 1} `;
+        values.push(attended);
+        whereClause = true;
+      }
+
+      if (rating !== undefined) {
+        if (whereClause) sql += 'AND ';
+        else sql += 'WHERE ';
+        sql += `ee.rating >= $${values.length + 1} `;
+        values.push(rating);
+        whereClause = true;
+      }
+
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
+
+  async getEnrollmentsId(id) {
+    const client = await pool.connect();
+    try {
+      let sql =
+        'SELECT u.first_name, u.last_name, u.username, ee.attended, ee.rating FROM users AS u INNER JOIN event_enrollments AS ee ON ee.id_user = u.id WHERE ee.id = $1';
+      const result = await client.query(sql, [id]);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
+
+  async addEvent(event) {
+    const client = await pool.connect();
+    try {
+      let sql =
+        'INSERT INTO public.events(name, description, id_event_category, id_event_location, start_date, duration_in_minutes, price, enabled_for_enrollment, max_assistance, id_creator_user, max_capacity)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);';
+      const values = [
+        event.name,
+        event.description,
+        event.id_event_category,
+        event.id_event_location,
+        event.start_date,
+        event.duration_in_minutes,
+        event.price,
+        event.enabled_for_enrollment,
+        event.max_assistance,
+        event.user.id,
+        event.max_capacity,
+      ];
+
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      client.release();
+    }
+  }
+
+  async updateEvent({
+    id,
+    name,
+    description,
+    max_assistance,
+    max_capacity,
+    price,
+    duration_in_minutes,
+  }) {
+    const client = await pool.connect();
+    try {
+      const sql = `
+			UPDATE events
+			SET name = $1, description = $2, max_assistance = $3, max_capacity = $4, price = $5, duration_in_minutes = $6
+			WHERE id = $7
+		  `;
+      const values = [
+        name,
+        description,
+        max_assistance,
+        max_capacity,
+        price,
+        duration_in_minutes,
+        id,
+      ];
+      await client.query(sql, values);
+    } finally {
+      client.release();
+    }
+  }
+
+  async deleteEvent(eventId) {
+    const client = await pool.connect();
+    try {
+      await client.query('DELETE FROM events WHERE id = $1', [eventId]);
+    } finally {
+      client.release();
+    }
+  }
 }
--- src/services/event-service.js
+++ src/services/event-service.js
@@ -2,48 +2,197 @@ import { response } from 'express';
 import EventRepository from '../repositories/event-repository.js';
 
 export default class EventService {
-	constructor() {
-		this.repo = new EventRepository();
-	}
+  constructor() {
+    this.repo = new EventRepository();
+  }
 
-	async getEvents(query) {
-		try {
-			const response = await this.repo.getEvents(query);
-			return response.length > 0
-				? [{ success: true, response: response }, 200]
-				: [{ success: false, message: 'No hay eventos para mostrar' }, 404];
-		} catch (error) {
-			throw new Error('Service error - getEvents(): \n' + error);
-		}
-	}
+  async getEvents(query) {
+    try {
+      const response = await this.repo.getEvents(query);
+      return response.length > 0
+        ? [{ success: true, response: response }, 200]
+        : [{ success: false, message: 'No hay eventos para mostrar' }, 404];
+    } catch (error) {
+      throw new Error('Service error - getEvents(): \n' + error);
+    }
+  }
 
-	async getEventDetails(id) {
-		try {
-			const response = await this.repo.getEventDetails(id);
-			return response.length > 0
-				? [{ success: true, response: response }, 200]
-				: [{ success: false, message: 'No existe un evento con ese ID' }, 404];
-		} catch (error) {
-			throw new Error('Service error - getEventsDetailes(id): \n' + error);
-		}
-	}
+  async getEventDetails(id) {
+    try {
+      const response = await this.repo.getEventDetails(id);
+      return response.length > 0
+        ? [{ success: true, response: response }, 200]
+        : [{ success: false, message: 'No existe un evento con ese ID' }, 404];
+    } catch (error) {
+      throw new Error('Service error - getEventsDetailes(id): \n' + error);
+    }
+  }
 
-	async getEnrollments(id, query) {
-		try {
-			const response = this.repo.getEnrollments(id, query);
-			return response.length > 0
-				? [{ success: true, response: response }, 200]
-				: [{ success: false, message: 'No hay enrollments para ese evento' }, 404];
-		} catch (error) {
-			throw new Error('Service error - getEnrollments(id, query): \n' + error);
-		}
-	}
+  async getEnrollments(id, query) {
+    try {
+      const response = await this.repo.getEnrollments(id, query);
+      return response.length > 0
+        ? [{ success: true, response: response }, 200]
+        : [
+            { success: false, message: 'No hay enrollments para ese evento' },
+            404,
+          ];
+    } catch (error) {
+      throw new Error('Service error - getEnrollments(id, query): \n' + error);
+    }
+  }
 
-	async addEvent({ body }) {
-		try {
-			await this.repo.addEvent(body);
-		} catch (error) {
-			throw new Error('Service error - addEvent({body}): \n' + error);
-		}
-	}
+  async addEvent({ body, user }) {
+    try {
+      const {
+        name,
+        description,
+        id_event_category,
+        id_event_location,
+        start_date,
+        duration_in_minutes,
+        price,
+        enabled_for_enrollment,
+        max_assistance,
+        max_capacity,
+      } = body;
+
+      if (name.length < 3 || description.length < 3) {
+        return [
+          {
+            success: false,
+            message:
+              'El nombre y la descripción deben tener al menos tres letras',
+          },
+          400,
+        ];
+      }
+
+      const eventLocation = await this.repo.getEventLocation(id_event_location);
+      const event_max_capacity = eventLocation[0].max_capacity;
+      if (max_assistance > event_max_capacity) {
+        return [
+          {
+            success: false,
+            message:
+              'La asistencia máxima no puede ser mayor que la capacidad máxima del lugar',
+          },
+          400,
+        ];
+      }
+      if (price < 0 || duration_in_minutes < 0) {
+        return [
+          {
+            success: false,
+            message:
+              'El precio y la duración en minutos no pueden ser valores negativos',
+          },
+          400,
+        ];
+      }
+
+      await this.repo.addEvent(body);
+      return [{ success: true, message: 'Evento creado correctamente' }, 201];
+    } catch (error) {
+      throw new Error('Service error - addEvent({body}): \n' + error);
+    }
+  }
+
+  async updateEvent({ body }) {
+    console.log(body);
+    try {
+      const [event] = await this.repo.getEventDetails(body.id);
+      if (!event) {
+        return [{ success: false, message: 'Evento no encontrado' }, 404];
+      }
+      if (event.id_creator_user !== body.user.id) {
+        return [
+          {
+            success: false,
+            message: 'No tienes permiso para editar este evento',
+          },
+          403,
+        ];
+      }
+
+      if (body.name.length < 3 || body.description.length < 3) {
+        return [
+          {
+            success: false,
+            message:
+              'El nombre y la descripción deben tener al menos tres letras',
+          },
+          400,
+        ];
+      }
+
+      if (body.max_assistance > event.event_location.max_capacity) {
+        return [
+          {
+            success: false,
+            message:
+              'La asistencia máxima no puede ser mayor que la capacidad máxima del lugar',
+          },
+          400,
+        ];
+      }
+
+      if (body.price < 0 || body.duration_in_minutes < 0) {
+        return [
+          {
+            success: false,
+            message:
+              'El precio y la duración en minutos no pueden ser valores negativos',
+          },
+          400,
+        ];
+      }
+
+      await this.repo.updateEvent(body);
+      return [
+        { success: true, message: 'Evento actualizado correctamente' },
+        200,
+      ];
+    } catch (error) {
+      throw new Error('Service error - updateEvent(): \n' + error);
+    }
+  }
+
+  async deleteEvent(eventId, user) {
+    try {
+      const [event] = await this.repo.getEventDetails(eventId);
+      if (!event) {
+        return [{ success: false, message: 'Evento no encontrado' }, 404];
+      }
+
+      if (event.id_creator_user !== user.id) {
+        return [
+          {
+            success: false,
+            message: 'No tienes permiso para eliminar este evento',
+          },
+          403,
+        ];
+      }
+
+      const enrollments = await this.repo.getEnrollmentsId(eventId, null);
+      if (enrollments.length > 0) {
+        return [
+          {
+            success: false,
+            message: 'No se puede eliminar el evento, hay usuarios registrados',
+          },
+          400,
+        ];
+      }
+
+      await this.repo.deleteEvent(eventId);
+      return [
+        { success: true, message: 'Evento eliminado correctamente' },
+        200,
+      ];
+    } catch (error) {
+      throw new Error('Service error - deleteEvent(): \n' + error);
+    }
+  }
 }
